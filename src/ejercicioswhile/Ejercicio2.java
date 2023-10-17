3c1dbd7f50563baa482b85f9776b0b6e5214cb1a
antoniovizarraga <145675344+antoniovizarraga@users.noreply.github.com>
2023-10-17 14:18:46 +0200

    Ejercicio 1 corregido y ejercicio 2 terminado

--- src/ejercicioswhile/Ejercicio2.java
+++ src/ejercicioswhile/Ejercicio2.java
@@ -1,13 +1,13 @@
 package ejercicioswhile;
 
-public class Ejercicio1 {
+public class Ejercicio2 {
 
 	public static void main(String[] args) {
 		int i = 0;
 		
 		do {
-			System.out.println(++i);
-		} while (i < 20);
+			System.out.println(i += 2);
+		} while(i < 200);
 
 	}
 
